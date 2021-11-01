849131161df8dc9e63f34aa14d445cc976fb2462
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-10-31 18:53:23 -0700

    Feat Defined Blogpost and Comment models

--- models/Blogpost.js
+++ models/Blogpost.js
@@ -7,7 +7,37 @@ const sequelize = require('../config/connection');
 class Blogpost extends Model {}
 
 Blogpost.init (
-
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        // Foreign key to User table
+        user_id:  {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            }
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blogpost'
+    }
 );
 
 module.exports = Blogpost;
--- models/Comment.js
+++ models/Comment.js
@@ -1,13 +1,44 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-// Define Blogpost model, any extends 
-// userAuth if auth needed
-
 class Comment extends Model {}
 
 Comment.init (
-
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        // Foreign key to User table
+        user_id:  {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            }
+        },
+        // Foreign key to Blogpost table
+        blogpost_id:  {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'blogpost',
+                key: 'id',
+            }
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
 );
 
 module.exports = Comment;
