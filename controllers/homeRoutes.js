2e8f06dcaf651804566cb3df04804d9ffe5798ba
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-10-31 23:36:52 -0700

    Feat: Build get route to generate homepage content from db & check session status.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,46 +2,48 @@ const router = require('express').Router();
 const {User, Blogpost} = require('../models')
 const userAuth = require('../utils/auth');
 
-// routes
-// some/all may require authentication check for access
-// userAuth if auth needed
-
 // The hompage displays all blogposts
 router.get('/', async (req, res) => {
     try {
         const blogpostData = await Blogpost.findAll({
-            //For each blogpost record returned, any associated records in the user model will also be return with that category
+            //For each Blogpost record, return the username attribute for any associated User record
             include: [
                 {
                     model: User,
                     attributes: ['username'],
                 }
             ],
-            // order: [
-            //     ['createdAt', 'DESC']
-            // ],
+            order: [
+                ['date_created', 'DESC'],
+            ],
         });
         // Serialize blogpostData for template to read
-        // const blogposts = blogpostData.map((blogpost) => blogpost.get({ plain: true }));
+        const blogposts = blogpostData.map((blogpost) => blogpost.get({ plain: true }));
+        // Testing format
+        console.log(blogposts);
         // render the homepage by passing in blogpost data and session flag to corresponding handlebars template
-        // res.render('homepage', {
-        //     blogposts,
-        //     logged_in: req.session.logged_in,
-        // });
-        res.status(200).json(blogpostData);
+        res.render('homepage', {
+            blogposts,
+            logged_in: req.session.logged_in,
+        });
+        // Testing route response content in Insomnia
+        // res.status(200).json(blogpostData);
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
-// // Render the dashboard page
-// router.get('/dashboard', userAuth, async (req, res) => {
-//     try {
-//         // logic for dashboard page contents
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+// Render the dashboard with all blogpost by a given user id
+router.get('/dashboard', userAuth, async (req, res) => {
+    try {
+        // logic for dashboard page contents
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Block user in log out state from accessing Dashboards
+
 
 // Render the login view for user to login
 router.get('/login', (req, res) => {
